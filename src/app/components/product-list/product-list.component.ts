4b4f293d9306f6404d343d6f864d1338d02db781
safanewlife <safanewlife>
2018-04-03 10:18:40 +0100

    Commit initial

---
+++ src/app/components/product-list/product-list.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { Product } from '../../domain/product';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent implements OnInit {
+
+data: Product[];
+
+  constructor() {
+    this.data = [
+      {code: 'P100', libelle: 'cafe', prixUnitaire: 500.5},
+      {code: 'P200', libelle: 'cafe', prixUnitaire: 500.5},
+      {code: 'P300', libelle: 'cafe', prixUnitaire: 500.5},
+      {code: 'P400', libelle: 'cafe', prixUnitaire: 500.5}
+    ];
+  }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/domain/product.ts
@@ -0,0 +1,7 @@
+export interface Product {
+
+  code: string;
+  libelle: string;
+  prixUnitaire: number;
+
+}
