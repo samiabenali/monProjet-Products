ad3847cf5097ebf1cafff86abf06af461c9405b7
Samia Ben Ali <samiabenali@sagem.com>
2018-04-03 10:25:29 +0100

    Ajout d'un nouveau produit

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -15,7 +15,8 @@ data: Product[];
       {code: 'P100', libelle: 'cafe', prixUnitaire: 500.5},
       {code: 'P200', libelle: 'cafe', prixUnitaire: 500.5},
       {code: 'P300', libelle: 'cafe', prixUnitaire: 500.5},
-      {code: 'P400', libelle: 'cafe', prixUnitaire: 500.5}
+      {code: 'P400', libelle: 'cafe', prixUnitaire: 500.5},
+      {code: 'P500', libelle: 'eau gazifiée', prixUnitaire: 500.5}
     ];
   }
 
